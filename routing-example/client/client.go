fd85cba16b27b16eedcfb481815eed19af6d5022
idiotcoffee <ishaanmavinkurve16@gmail.com>
2026-09-22 21:06:57 +0530

    practice of different types of gRPC servers - practiced unary and server streaming, yet to implement client streaming and bi-directional streaming!

---
+++ routing-example/client/client.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/idiotcoffee/gRPC-learning/router"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient(
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewRouteGuideClient(conn)
+	//unary
+	feature, err := client.GetFeature(
+		context.Background(),
+		&pb.Point{
+			Latitude:  406411633,
+			Longitude: -741722051,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Feature: %s", feature.Name)
+
+	// server-streaming
+	stream, err := client.ListFeatures(context.Background(), &pb.Rectangle{
+		Lo: &pb.Point{
+			Latitude:  400000000,
+			Longitude: -750000000,
+		},
+		Hi: &pb.Point{
+			Latitude:  420000000,
+			Longitude: -730000000,
+		}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		feature, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf(
+			"Feature: %s (%d, %d)",
+			feature.Name,
+			feature.Location.Latitude,
+			feature.Location.Longitude,
+		)
+	}
+}
